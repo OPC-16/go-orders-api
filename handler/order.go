9644e7341babaa5ce6bab3543dcfe9d60bcb8f1e
OPC-16 <chavan.p.omkar@gmail.com>
2023-09-24 12:08:22 +0530

    Add CRUD handler methods in order.go

---
+++ handler/order.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct {}
+
+/* These are handler methods which get called on specific routes, these routes are defined in application/routes.go */
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Create an order")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("List all orders")
+}
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Get an order by ID")
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Update an order by ID")
+}
+
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Delete an order by ID")
+}
